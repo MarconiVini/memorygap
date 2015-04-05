cb4fb276707238fccf5773ba9adb1651da43fe05
Marcos Santini <cardosounicamp@gmail.com>
2015-04-04 23:28:28 -0300

    added all test framework

---
+++ spec/factories/occultis_label.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :occultis_label do
+
+  end
+end
---
+++ spec/models/occultis_labels_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+RSpec.describe OccultisLabel, :type => :model do
+  it "orders by last name" do
+    
+  end
+end
